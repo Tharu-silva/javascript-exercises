19c6ce6561c49882775745ee67e969feeca8d1d6
Tharu <u7145170@anu.edu.au>
2022-04-16 19:59:17 +1000

    Finished ex. 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let revString = []
+    for (let i = string.length - 1; i > -1; i--){
+        revString.push(string[i])
+    }
+    return revString.join('')
 };
 
 // Do not edit below this line
