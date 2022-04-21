6ca4ab27208fd61dd00dbac09322cd644ba7fd2e
Tharu <u7145170@anu.edu.au>
2022-04-21 14:26:24 +1000

    Finished ex. 10\

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+//Define two elements then add them together and make the prev element the result of that addition
+    if (x < 0) {
+        return "OOPS"
+    }
+    prev = 0
+    next = 1
+    for (let i = 0; i < (x-1); i++){
+        next = prev + next;
+        //Since we added next in the line above
+        prev = next - prev;
+    }
+    return next
 };
 
 // Do not edit below this line
