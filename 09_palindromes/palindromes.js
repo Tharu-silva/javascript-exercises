dab86f163d93bd921b14d0569df9b0813c7ddd2d
Tharu <u7145170@anu.edu.au>
2022-04-21 14:15:36 +1000

    Finished ex. 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    filteredWord = word.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "").toLowerCase();
+    reveseredWord = reverseString(filteredWord);
+    return (filteredWord === reveseredWord);
 
+};
+
+const reverseString = function(string) {
+    let revString = []
+    for (let i = string.length - 1; i > -1; i--){
+        revString.push(string[i])
+    }
+    return revString.join('')
 };
 
 // Do not edit below this line
